dc0cf620e2e663cd91eff2d162bab170687176f1
AlexCaojc <13521233954@163.com>
2018-04-05 22:36:47 +0800

    use try-catch to make sure join() certain to be called

--- main.cpp
+++ main.cpp
@@ -10,12 +10,18 @@ int main()
 {
     std::thread t1(function_1);
 
-//    t1.join();///主线程会等待这里的t1线程结束后再运行，t1和主线程是两个完全互不影响的线程
-
-//    t1.detach();///一个线程一旦detach，就不能join了
-    if (t1.joinable())
+    try
+    {
+        for (int i = 0; i <100; i++)
+        {
+            std::cout << "from main:" << i << std::endl;
+        }
+    }
+    catch (...)
     {
         t1.join();
+        throw;
     }
+    t1.join();
     return 0;
 }
