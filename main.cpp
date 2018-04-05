2572a12c0f489aeb501c51808529117624a21510
AlexCaojc <13521233954@163.com>
2018-04-05 22:51:27 +0800

    use object to create thread

--- main.cpp
+++ main.cpp
@@ -6,10 +6,21 @@ void function_1()
     std::cout << "www.oxox.work" << std::endl;
 }
 
+class Factor
+{
+public:
+    void operator() ()
+    {
+        for (int i = 0; i > -100; i--)
+            std::cout << "from t1: " << i << std::endl;
+    }
+};
+
 int main()
 {
-    std::thread t1(function_1);
-
+//    Factor fct;
+//    std::thread t1(fct);///使用对象创建线程,t1和主线程交互运行
+    std::thread t1((Factor()));
     try
     {
         for (int i = 0; i <100; i++)
