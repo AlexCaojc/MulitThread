9d8dd03019204cbc6532f598ce5f90ecca745232
AlexCaojc <13521233954@163.com>
2018-04-05 22:27:29 +0800

    odify

--- main.cpp
+++ main.cpp
@@ -10,10 +10,9 @@ int main()
 {
     std::thread t1(function_1);
 
-    ///主线程会等待这里的t1线程结束后再运行
-    ///t1和主线程是两个完全互不影响的线程
-//    t1.join();
-    t1.detach();///一个线程yi但detach，就不能join了
+//    t1.join();///主线程会等待这里的t1线程结束后再运行，t1和主线程是两个完全互不影响的线程
+
+//    t1.detach();///一个线程一旦detach，就不能join了
     if (t1.joinable())
     {
         t1.join();
