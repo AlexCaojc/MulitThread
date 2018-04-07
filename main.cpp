ca63bc873503cb44326038ef8acea6d57b28d20a
AlexCaojc <13521233954@163.com>
2018-04-08 00:03:32 +0800

    使用std::ref传递引用，但是会导致线程间分享内存，引起数据竞争，但是值传递和std::move不知道为什么编译不过去

--- main.cpp
+++ main.cpp
@@ -1,11 +1,7 @@
+
 #include <iostream>
 #include <thread>
 #include <string>
-
-void function_1()
-{
-    std::cout << "www.oxox.work" << std::endl;
-}
 
 class Factor
 {
@@ -20,10 +16,12 @@ public:
 int main()
 {
     ///在不同线程中传递数据采用的是值传递，即使函数参数为引用形式
-    std::string s = "i love www.oxox.work";
-    std::thread t1((Factor()), s);
+    std::string msg = "i love www.oxox.work";
+    std::thread t1((Factor()), std::ref(msg));///传递引用，但是会导致线程间分享内存，引起数据竞争
+//    std::thread t1((Factor()), std::move(msg));///将msg从主线程移动到子线程,但是编译不过，暂时没有找到原因
+//    std::thread t1((Factor()), msg);///将msg从主线程移动到子线程，但是编译不过，暂时没有找到原因
 
     t1.join();
-    std::cout << "from main:" << s << std::endl;
+    std::cout << "from main:" << msg << std::endl;
     return 0;
 }
