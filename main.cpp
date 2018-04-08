85fcee70ebf1abdfe95040feea7819c275218a8e
AlexCaojc <13521233954@163.com>
2018-04-08 22:35:16 +0800

    当前线程调用结束之后其他线程才可以调用,但是当程序抛出异常时可能会导致程序死锁

--- main.cpp
+++ main.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
 #include <thread>
 #include <string>
+#include <mutex>
+
+
+std::mutex mu;
+
+void shared_print(std::string msg, int id)
+{
+    ///使用互斥对象进行同步资源的访问解决资源竞争问题,一个线程占用资源是其他线程需要等待，
+    /// 当前线程调用结束之后其他线程才可以调用
+    /// 但是当程序抛出异常时可能会导致程序死锁
+    mu.lock();
+    std::cout << msg << id << std::endl;
+    mu.unlock();
+}
 
 void function_1()
 {
     for (int i = 0; i < 100; i++)
     {
-        std::cout << "from function_1: i = " << i << std::endl;
+        shared_print("from function_1: i = ", i);
     }
 }
 
@@ -16,10 +30,8 @@ int main()
 
     for (int i = 0; i < 100; i++)
     {
-        std::cout << "from main: i = " << i << std::endl;
+        shared_print("from main: i = ", i);
     }
-
-    ///此时t1和主线程同时竞争std::cout资源，导致数据输出时乱的
 
     t1.join();
     return 0;
