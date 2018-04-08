d0cf493e99e109f3ae5fddb28e41b103cd1e1b4a
AlexCaojc <13521233954@163.com>
2018-04-08 22:15:10 +0800

    t1和主线程同时竞争std::cout资源，导致数据输出时乱的

--- main.cpp
+++ main.cpp
@@ -1,30 +1,26 @@
-
 #include <iostream>
 #include <thread>
 #include <string>
 
-class Factor
+void function_1()
 {
-public:
-    void operator() (std::string &msg)
+    for (int i = 0; i < 100; i++)
     {
-        std::cout << "from t1: " << msg << std::endl;
-        msg = "i love XuHuanDaXue";
+        std::cout << "from function_1: i = " << i << std::endl;
     }
-};
+}
 
 int main()
 {
-    ///在不同线程中传递数据采用的是值传递，即使函数参数为引用形式
-    std::string msg = "i love www.oxox.work";
-    std::thread t1((Factor()), std::ref(msg));///传递引用，但是会导致线程间分享内存，引起数据竞争
+    std::thread t1(function_1);
 
-    std::thread t2 = std::move(t1);///线程对象只能被移动而不能被复制
+    for (int i = 0; i < 100; i++)
+    {
+        std::cout << "from main: i = " << i << std::endl;
+    }
 
-    std::cout << "the main id = "<<std::this_thread::get_id() <<std::endl;
-    std::cout << "the t2 id = " << t2.get_id() << std::endl;
+    ///此时t1和主线程同时竞争std::cout资源，导致数据输出时乱的
 
-    t2.join();
-    std::cout << "the most cpu core num = " << std::thread::hardware_concurrency() << std::endl;
+    t1.join();
     return 0;
 }
