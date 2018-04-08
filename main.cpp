ae78d4a0f38b4c66df1e8eaaeb67d9f93b0444d3
AlexCaojc <13521233954@163.com>
2018-04-09 00:02:04 +0800

    在一个类的函数内使用互斥构造lock_guard，同时维护资源，这样就不可能在类外对资源进行单独操作,保证资源不会造成竞争

--- main.cpp
+++ main.cpp
@@ -2,40 +2,53 @@
 #include <thread>
 #include <string>
 #include <mutex>
+#include <fstream>
 
-
-std::mutex mu;
-
-void shared_print(std::string msg, int id)
+class LogFile
 {
-    ///类 lock_guard 是互斥封装器，为在作用域块期间占有互斥提供便利 RAII 风格机制。
-    ///创建 lock_guard 对象时，它试图接收给定互斥的所有权。控制离开创建 lock_guard 对象的作用域时，销毁 lock_guard 并释放互斥。
-    ///lock_guard 类不可复制。
-    ///当guard对象析构时，不管程序有没有异常，mu都会被解锁
-    /// Resource Acquisition Is Initialization
-    /// 在资源获取的时候将其封装在某类的object中，
-    /// 利用"栈资源会在相应object的生命周期结束时自动销毁"来自动释放资源，
-    /// 即将资源释放写在析构函数中。所以这个RAII其实就是和智能指针的实现是类似的
-    std::lock_guard<std::mutex> guard(mu);
-    ///但是cout资源并没有完全在互斥对象的保护下，因为cout是一个全局对象，其他程序仍然可以在不加锁的情况下使用cout
-    std::cout << msg << id << std::endl;
-}
+public:
+    LogFile() {
+        f.open("log.txt");
+    }
 
-void function_1()
+    void share_print(std::string id, int value)
+    {
+        ///在一个类的函数内使用互斥构造lock_guard，同时维护资源，这样就不可能在类外对资源进行单独操作
+        /// 保证资源不会造成竞争
+        std::lock_guard<std::mutex> locker(m_mutex);
+        f << "from" << id << ":" <<value << std::endl;
+    }
+
+    ///但你必须维护这个保护等级，不能返回资源的引用到类外，同时不能将资源作为函数的参数传递出去
+#if 0
+    std::ofstream &getOfstreanm() { return f; }
+    void processf(void fun(ofstream&))
+    {
+        fun(f);
+    }
+
+#endif
+
+private:
+    std::mutex m_mutex;
+    std::ofstream f;
+
+};
+void function_1(LogFile &log)
 {
     for (int i = 0; i < 100; i++)
     {
-        shared_print("from function_1: i = ", i);
+        log.share_print("from function_1: i = ", i);
     }
 }
 
 int main()
 {
-    std::thread t1(function_1);
-
+    LogFile log;
+    std::thread t1((function_1), std::ref(log));
     for (int i = 0; i < 100; i++)
     {
-        shared_print("from main: i = ", i);
+        log.share_print("from main: i = ", i);
     }
 
     t1.join();
